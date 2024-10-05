f18d3dc8a12ef4b8ce4e370ea98ed0338e920851
robloxlover44 <danza.davydov@gmail.com>
2024-10-05 20:41:21 +0300

    added music

---
+++ Assets/_Source/BackgroundMusic.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    private static BackgroundMusic instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/_Source/ButtonFX.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFX : MonoBehaviour
+{
+    public AudioSource myFX;
+    public AudioClip hoverFX;
+    public AudioClip clickFX;
+
+    public void HoverSound()
+    {
+        myFX.PlayOneShot(hoverFX);
+    }
+
+    public void Clicksound()
+    {
+        myFX.PlayOneShot(clickFX);
+    }
+}
