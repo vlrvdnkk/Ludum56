8193ba7e76b625fff2011cb032fdf37c93a119d8
Алина Красильникова <KrasilnikovaAA21@st.ithub.ru>
2024-10-05 13:54:59 +0300

    Added folding

--- Assets/_Source/SceneChanger.cs
+++ Assets/_Source/SceneChanger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class levelcontroler : MonoBehaviour
+public class SceneChanger : MonoBehaviour
 {
     public int scene;
     public void changescene()
