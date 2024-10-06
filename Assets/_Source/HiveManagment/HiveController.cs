461884e6c9ff0c179679fdd0c1898af72716010f
Алина Красильникова <KrasilnikovaAA21@st.ithub.ru>
2024-10-06 19:24:14 +0300

    Transition from hives added

--- Assets/_Source/HiveManagment/HiveController.cs
+++ Assets/_Source/HiveManagment/HiveController.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class HiveController : MonoBehaviour
 {
-    [SerializeField] private string sceneToLoad;
+    [SerializeField] private int sceneToLoad;
     [SerializeField] private TextMeshProUGUI interactionText;
     [SerializeField] private string hiveKey;
     [SerializeField] private bool isFinalHive = false;
@@ -16,57 +16,45 @@ public class HiveController : MonoBehaviour
 
     private void Start()
     {
-        // Проверяем, был ли этот улей уже активирован
         isCompleted = PlayerPrefs.GetInt(hiveKey, 0) == 1;
-
-        // Отключаем триггерную зону, если улей уже пройден
-        if (isCompleted)
-        {
-            interactionText.gameObject.SetActive(false);
-            gameObject.SetActive(false);  // Отключаем улей, если он уже был активирован
-        }
-        else
-        {
-            interactionText.gameObject.SetActive(false);  // Скрываем текст "E" в начале
-        }
+        interactionText.gameObject.SetActive(false);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D (Collider2D other)
     {
-        Debug.Log("ghjk");
-        // Если объект входит в триггерную зону и принадлежит слою игрока
         if (IsPlayerLayer(other.gameObject) && !isCompleted)
         {
-            if (isFinalHive && !CheckRequiredHives())  // Если это пятый улей и не все предыдущие пройдены
+            if (isFinalHive && !CheckRequiredHives())
             {
                 interactionText.text = "You must complete all other hives first!";
                 interactionText.gameObject.SetActive(true);
             }
             else
             {
-                interactionText.text = "Press E to enter";  // Обычный текст "E"
+                interactionText.text = "Press E to enter";
                 interactionText.gameObject.SetActive(true);
                 isPlayerInTrigger = true;
             }
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit2D(Collider2D other)
     {
-        // Если игрок выходит из триггерной зоны
         if (IsPlayerLayer(other.gameObject))
         {
-            interactionText.gameObject.SetActive(false);  // Скрываем текст "E"
+            if (interactionText != null)
+            {
+                interactionText.gameObject.SetActive(false);
+            }
             isPlayerInTrigger = false;
         }
     }
 
     private void Update()
     {
-        // Проверяем, нажата ли кнопка E и находится ли игрок в триггерной зоне
         if (isPlayerInTrigger && Input.GetKeyDown(KeyCode.E))
         {
-            if (isFinalHive && CheckRequiredHives())  // Проверяем условия для пятого улья
+            if (isFinalHive && CheckRequiredHives())
             {
                 LoadHiveScene();
             }
@@ -79,39 +67,37 @@ public class HiveController : MonoBehaviour
 
     private void LoadHiveScene()
     {
-        // Переход на сцену, если улей ещё не пройден
         if (!isCompleted)
         {
             SceneManager.LoadScene(sceneToLoad);
         }
     }
 
-    // Этот метод вызывается, когда игрок возвращается из сцены улья
     public void CompleteHive()
     {
         isCompleted = true;
-        PlayerPrefs.SetInt(hiveKey, 1);  // Сохраняем прогресс в PlayerPrefs
+        PlayerPrefs.SetInt(hiveKey, 1);
         PlayerPrefs.Save();
 
-        // Отключаем улей и его триггерную зону
-        interactionText.gameObject.SetActive(false);
-        gameObject.SetActive(false);
+        Collider hiveCollider = GetComponent<Collider>();
+        if (hiveCollider != null)
+        {
+            hiveCollider.enabled = false;
+        }
     }
 
-    // Проверка, что все нужные ульи пройдены
     private bool CheckRequiredHives()
     {
         foreach (string requiredHiveKey in requiredHives)
         {
             if (PlayerPrefs.GetInt(requiredHiveKey, 0) != 1)
             {
-                return false;  // Если хотя бы один улей не пройден, возвращаем false
+                return false;
             }
         }
-        return true;  // Все предыдущие ульи пройдены
+        return true;
     }
 
-    // Проверка, находится ли объект на слое игрока
     private bool IsPlayerLayer(GameObject obj)
     {
         return (playerLayer.value & (1 << obj.layer)) > 0;
