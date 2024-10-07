8d15bf82f533fefa5e8e37ae0f3415c8fb34198c
robloxlover44 <danza.davydov@gmail.com>
2024-10-07 17:45:09 +0300

    bugs fixed flykiller

--- Assets/_Source/FlyKiller/FlyKillerGameManager.cs
+++ Assets/_Source/FlyKiller/FlyKillerGameManager.cs
@@ -5,8 +5,10 @@ public class FlyKillerGameManager : MonoBehaviour
 {
     public static FlyKillerGameManager Instance;
     [SerializeField] private int totalFlies = 10;
-    [SerializeField] private TextMeshProUGUI gameOverText;
+    [SerializeField] private GameObject gameOverText;
     [SerializeField] private GameObject exitButton;
+    [SerializeField] private AudioSource WinSound;
+    [SerializeField] private AudioSource FlyKillSound;
 
     private int fliesKilled = 0;
 
@@ -27,6 +29,7 @@ public class FlyKillerGameManager : MonoBehaviour
     public void FlyKilled()
     {
         fliesKilled++;
+        FlyKillSound.Play();
 
         if (fliesKilled >= totalFlies)
         {
@@ -38,5 +41,6 @@ public class FlyKillerGameManager : MonoBehaviour
     {
         gameOverText.gameObject.SetActive(true);
         exitButton.gameObject.SetActive(true);
+        WinSound.Play(); // Проигрываем звук победы
     }
 }
