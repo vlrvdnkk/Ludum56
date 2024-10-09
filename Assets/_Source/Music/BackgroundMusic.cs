bae599bb93cef54ac9e7fb705a9a236b747bf804
Алина Красильникова <KrasilnikovaAA21@st.ithub.ru>
2024-10-09 16:58:06 +0300

    Fixed music bugs

--- Assets/_Source/Music/BackgroundMusic.cs
+++ Assets/_Source/Music/BackgroundMusic.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BackgroundMusic : MonoBehaviour
 {
     private static BackgroundMusic instance = null;
+
+    [SerializeField] private int sceneToDestroyMusic;
 
     void Awake()
     {
@@ -12,6 +15,24 @@ public class BackgroundMusic : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
         else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.buildIndex == sceneToDestroyMusic)
         {
             Destroy(gameObject);
         }
